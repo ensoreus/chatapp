ccbfee687d0bfe92036d1bf377465974861fd6db
Pylyp Maliuta <pylyp.maliuta@stuzo.com>
2024-01-28 20:23:54 +0200

    contact send

--- chatapp/ReverseJoker.swift
+++ chatapp/ReverseJoker.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 class ReverseJoker: Contact {
+    let messagesToLoad: [String] = [
+        "lorem ipsum dolor sit amet aliquid proident fuga.",
+        "amet possimus fuga, propter ut. passim quam quo",
+        "orem ipsum dolor sit amet enim velit laudantium.",
+        "maximas vel quo satellitibus accusantium. quibusdam voluptatum dominatrix eveniet non. eos minima est corporis repellat. vero mollit odit",
+        "dolore commodo. id quasi ea, tempora corporis. laborum nobis non, consequuntur humani. labore videlicet do officia",
+        "lorem ipsum dolor sit amet fugit adipisicing in velit congregavit. aspernatur eius rationem, inducens utilem. unde sit libero rerum inventore",
+        "lorem ipsum dolor sit amet esset reclamantes esse."
+    ]
+
+    override init(name: String, owner: Bool) {
+        super.init(name: name, owner: owner)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [weak self] in
+            self?.messagesToLoad.forEach { message in
+                self?.send(text: message)
+            }
+        }
+    }
     override func send(text: String) {
         channel?.broadcast(text: text, sender: self)
     }
@@ -15,4 +33,6 @@ class ReverseJoker: Contact {
     override func receive(text: String, sender: Contact) {
         send(text: String(text.reversed()))
     }
+
+
 }
